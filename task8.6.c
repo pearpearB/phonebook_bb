2a6e68c0f6542c6b10c88dfe018be11ce9399be2
pearpearB <smilyoung>
2021-08-29 16:44:07 +0900

    task8.6 phonebook done

--- task8.6.c
+++ task8.6.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct NODE{
-    struct NODE *next;
+struct ph_book{
+    struct ph_book *next;
     char *name;
     int age;
     char *number;
@@ -10,34 +10,38 @@ struct NODE{
 
 int main()
 {
-    struct NODE *head = malloc(sizeof(struct NODE));
-
-    struct NODE *node1 = malloc(sizeof(struct NODE));
-    head->next = node1;
-    node1->name = "jueun";
-    node1->age = 27;
-    node1->number = "01099995555";
+    int i = 1;
     
-    struct NODE *node2 = malloc(sizeof(struct NODE));
-    node1->next = node2;
-    node2->name = "taeho";
-    node2->age = 25;
-    node2->number = "01042424242";
+    struct ph_book *head = malloc(sizeof(struct ph_book));
 
-    node2->next = NULL;
+    struct ph_book *ph_book1 = malloc(sizeof(struct ph_book));
+    head->next = ph_book1;
+    ph_book1->name = "jueun";
+    ph_book1->age = 27;
+    ph_book1->number = "01099995555";
+    
+    struct ph_book *ph_book2 = malloc(sizeof(struct ph_book));
+    ph_book1->next = ph_book2;
+    ph_book2->name = "taeho";
+    ph_book2->age = 25;
+    ph_book2->number = "01042424242";
 
-    struct NODE *curr = head->next;
+    ph_book2->next = NULL;
+
+    struct ph_book *curr = head->next;
 
     while (curr != NULL)
     {
+        printf("phonebook %d.\n", i);
         printf("name : %s\n", curr->name);
         printf("age : %d\n", curr->age);
         printf("number : %s\n", curr->number);
         curr = curr->next;
+        i++;
     }
 
-    free(node2);
-    free(node1);
+    free(ph_book2);
+    free(ph_book1);
     free(head);
 
     return 0;
